0d29a441fdb89b3a448f34a65206d09b6afe269d
Dominik Zogg <dominik.zogg@gmail.com>
2015-10-28 21:12:50 +0100

    initial commit

---
+++ ansible/ansible-on-guest.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Copy resolv.conf (prevent dns problems)
+cp -f /vagrant/vagrant-php/ansible/resolv.conf /etc/resolv.conf
+
+# Copy sources.list
+cp -f /vagrant/vagrant-php/ansible/sources.list /etc/apt/sources.list
+
+# Update apt information
+apt-get update > /dev/null 2>&1
+
+# Install ansible, https support for apt
+apt-get install -y ansible aptitude apt-transport-https > /dev/null 2>&1
+
+export PYTHONUNBUFFERED=1
+
+# Run the playbook.
+echo "Running Ansible provisioner defined in Vagrantfile."
+echo "extra-vars: " $2
+ansible-playbook -i localhost, --connection=local --extra-vars=${2} /vagrant/vagrant-php/${1}
